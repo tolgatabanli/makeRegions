1a30f306ba4d9dec765a969c233833101e54e912
tolgatabanli <tolgatabanli@gmail.com>
2025-06-04 21:35:27 +0200

    changed out folder name to be compatible with the kathi's out

--- run_make_windows.sh
+++ run_make_windows.sh
@@ -1,17 +1,17 @@
 
 # minimal arguments with gtf
 echo "---minimal arguments with gtf"
-./make_windows.R --gtf data/example.gtf --upstream 1000 --downstream 2000 --out out/minimal_gtf.bed
+./make_windows.R --gtf data/example.gtf --upstream 1000 --downstream 2000 --out out_tolga/minimal_gtf.bed
 
 # minimal arguments with bed
 echo "---minimal arguments with bed"
-./make_windows.R --bed data/example_annotation_1000up_2000down.bed --upstream 1000 --downstream 2000 --out out/minimal_bed.bed
+./make_windows.R --bed data/example_annotation_1000up_2000down.bed --upstream 1000 --downstream 2000 --out out_tolga/minimal_bed.bed
 
 # filter wrt feature="gene" biotype="lncRNA"
 echo "---filter wrt feature=\"gene\" biotype=\"lncRNA\""
-./make_windows.R --gtf data/example.gtf --upstream 1000 --downstream 2000 --out out/example_gtf_gene_lncRNA.bed --feature gene --biotype lncRNA
+./make_windows.R --gtf data/example.gtf --upstream 1000 --downstream 2000 --out out_tolga/example_gtf_gene_lncRNA.bed --feature gene --biotype lncRNA
 
 # edge case with edge make_windows
 echo "---edge case with edge make_windows"
-./make_windows.R --gtf data/example_edges.gtf --upstream 1000 --downstream 2000 --out out/example_gtf_gene_lncRNA_edges.bed --feature gene --biotype lncRNA
+./make_windows.R --gtf data/example_edges.gtf --upstream 1000 --downstream 2000 --out out_tolga/example_gtf_gene_lncRNA_edges.bed --feature gene --biotype lncRNA
 
