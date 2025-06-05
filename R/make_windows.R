82808d435b27bbe5148d6cfc1b5640aeafe1dcd9
tolgatabanli <tolgatabanli@gmail.com>
2025-06-05 20:03:09 +0200

    first working copy of packaged version

---
+++ R/make_windows.R
@@ -0,0 +1,114 @@
+utils::globalVariables(c("gtf", "bed", "name", "path_to_out", "start", "end", "strand", "seqnames", "score", "gene_id", "input"))
+
+#' Create genomic regions with with given upstream and downstream sizes
+#'
+#' @param input GTF or BED file
+#' @param upstream Upstream size
+#' @param downstream Downstream size
+#' @param path_to_output (Optional) File path to write output. Creates dirs if they do not exist
+#' @param feature (Optional) Filter by given feature such as gene, transcript...
+#' @param biotype (Optional) Filter by given biotype, if exists in the input GTF, such as lncRNA, pseudogene...
+#' @param position (Optional) Specify 'start' or 'end' to build the window around.
+#' If not specified, the start and end coordinates are extended with upstream and downstream sized in corresponding way.
+#'
+#' @returns Annotated .bed file with windows specified.
+#' @export
+#'
+#' @examples TODO
+make_windows <- function(input_file, upstream, downstream,
+                         path_to_output = NULL,
+                         feature = NULL, biotype = NULL, position = NULL) {
+  score <- NA
+  # Argument checks
+  if (missing(input_file) || missing(upstream) || missing(downstream)) {
+    stop("Missing required arguments: --upstream, --downstream")
+  }
+  if(endsWith(input_file, ".gtf")) {
+    input_file <- rtracklayer::import(input_file) %>%
+      as.data.frame()
+  } else if (endsWith(input_file, ".bed")) {
+    input_file <- rtracklayer::import(input_file) %>%
+      as.data.frame(strings) %>%
+      dplyr::rename(gene_id = name)
+  } else {
+    stop("Provided input file is of wrong format. Should: .gtf or .bed")
+  }
+
+  # optional argument check
+  if(!is.null(position) && !(position %in% c("start", "end"))) {
+    stop("Position argument should be either 'start' or 'end' !")
+  }
+  # TODO: Check path_to_out validity
+
+  # Create the directory path in out if they don't exist
+  if (!is.null(path_to_output)) {
+    dir_path <- dirname(path_to_out)
+    if (dir_path != "." && !dir.exists(dir_path)) {
+      dir.create(dir_path, recursive = TRUE)
+    }
+  }
+
+  # Create the .bed file
+  annotation <- input_file %>%
+
+    # Filter feature (gene, variaton, ...) and biotype (protein_coding, lncRNA, ...)
+    # columns are quoted
+    dplyr::filter(using_if_given(., "gene_biotype", biotype)) %>%
+    dplyr::filter(using_if_given(., "type", feature)) %>%
+
+    # if position argument is given, set both coordinates to the given position
+    dplyr::mutate(start = ifelse(!is.null(position) && position == "end", end, start),
+           end = ifelse(!is.null(position) && position == "start", start, end)) %>%
+
+    # Calculate upstream-downstream windows
+    dplyr::mutate(start = ifelse(strand == "+", start-1-upstream, start-1-downstream),
+           end = ifelse(strand == "+", end+downstream, end+upstream)) %>%
+
+    # if score not found, fill with dot in .bed annotation
+    dplyr::mutate(score = ifelse(is.na(score), ".", score)) %>%
+
+    # remove regions where coordinates go over the edge
+    dplyr::filter({
+      negative_detected <- start < 0 | end < 0
+      if (any(negative_detected, na.rm = TRUE)) message("Negative coordinates filtered out!")
+      !negative_detected
+    }) %>%
+    dplyr::select(seqnames, start, end, gene_id, score, strand)
+
+  if(!is.null(path_to_output)) {
+    annotation %>%
+      readr::write_tsv(file.path(path_to_out), col_names = F)
+  }
+
+  return(as.data.frame(annotation))
+}
+
+
+
+# HELPERS
+
+# handle filter operations
+using_if_given <- function(df, column_name, value) {
+  # df:           the data frame
+  # column_name:  the name of the column as a string (e.g., "biotype")
+  # value:        the value to filter on (e.g., "protein_coding")
+
+  # Ignore 1: If value not provided
+  if (is.null(value) || is.na(value)) {
+    cat("Argument not given:", deparse(substitute(value)), # backtracking for variable name
+        "- continuing without filter.\n")
+    return(TRUE)
+  }
+
+  # Ignore 2: If col does not exist
+  if (!(column_name %in% colnames(df))) {
+    cat("The input data does not have column:", column_name,
+        "- continuing without filter.\n")
+    return(TRUE)
+  }
+
+  # Success: Perform filter
+  return(df[[column_name]] == value)
+}
+
+
---
+++ README.md
@@ -0,0 +1,26 @@
+
+# makeregions
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of makeregions is to ...
+
+## Installation
+
+You can install the development version of makeregions from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("tolgatabanli/makeregions_windows")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(makeregions)
+## basic example code
+```
+
