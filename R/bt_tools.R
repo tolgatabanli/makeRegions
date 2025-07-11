6d37b5e9869c0e88e94d38c033b1e95e7cd6fb4d
tolgatabanli <tolgatabanli@gmail.com>
2025-07-12 01:48:48 +0200

    added stranded arg which creates two strand-specific files and more compact code

--- R/bt_tools.R
+++ R/bt_tools.R
@@ -33,42 +33,81 @@ get_bedtools_path <- function() {
 #' Get the genome coverage using bedtools
 #'
 #' @description
-#' Handles following command: bedtools genomecov -ibam input_file -bg > output_file.
-#' Used for its side effect, creating coverage file.
+#' Handles the following command:
+#'  bedtools genomecov -ibam input_file -bg (-strand strand) > output_file.
+#' Creates or returns coverage file.
 #'
 #'
 #' @param input_bed Path to input BED file.
 #' @param genome_file Path to genome file.
-#' @param output_file Optional path to output file.
+#' @param output_file [optional] path to output file.
+#' The extension in the given name will be stripped.
+#' If stranded = TRUE, then two output files with "_pos" and "_neg" appended
+#' will be created.
+#' @param stranded logical. Should output be split into strand-specific two files?
+#' Uses -strand argument of genomecov.
+#' Default is FALSE.
 #'
-#' @return If output file path given, a string of its path is returned,
-#' otherwise a data table of the result captured from the command is returned.
+#'
+#' @return If output file given, its path or a vector of paths (if stranded) is returned.
+#' Otherwise, a data table of the result captured from the command, or
+#' if stranded, a list of two data tables: (+,-).
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #'   set_bedtools_path("/usr/bin/bedtools")
-#'   bt_genomecov("bamfile.bam", "output.bedgraph")
+#'   bt_genomecov("bamfile.bam", "output.bedgraph", stranded = TRUE)
 #' }
-bt_genomecov <- function(input_bam, output_file = NULL) {
+bt_genomecov <- function(input_bam, output_file = NULL, stranded = FALSE) {
   # Check input file type
   stopifnot(endsWith(input_bam, ".bam"))
-  stopifnot()
+  # 0: not stranded, 1: regular, -1: reverse stranded
+
+  # stranded = FALSE: not stranded
+  # stranded = TRUE: strand-specific output (split into + and -) -> two files
 
   bedtools <- get_bedtools_path()
-  args <- c("genomecov", "-ibam", input_bam, "-bg")
+  base_args <- c("genomecov", "-ibam", input_bam, "-bg")
+
+  # Set strand mapping if given
+  strand_map <- c(pos = "+", neg = "-")
 
   if (!is.null(output_file)) {
-    dir <- dirname(output_file)
-    if (!dir.exists(dir)) {
-      dir.create(dir, recursive = TRUE)
+    output_file <- sub("\\.bedgraph$", "", output_file)
+    dir.create(dirname(output_file), showWarnings = FALSE, recursive = TRUE)
+
+    # if stranded -> split the result into strands
+    if (stranded) {
+
+      for (s in names(strand_map)) {
+        args <- c(base_args, "-strand", strand_map[[s]])
+        file <- paste0(output_file, "_", s, ".bedgraph")
+        processx::run(bedtools, args, stdout = file)
+      }
     }
 
-    processx::run(bedtools, args, stdout = output_file)
+    # if not stranded do one call
+    else {
+      processx::run(bedtools, base_args, stdout = paste0(output_file, ".bedgraph"))
+    }
+
     return(invisible(output_file))
-  } else {
-    res <- processx::run(bedtools, args)
-    # stdout is a tab separated string, should parse with fread
-    return(data.table::fread(res$stdout))
   }
+
+  # No output file -> return data in R
+  if (stranded) {
+    result <- lapply(strand_map, function(s) {
+      args <- c(base_args, "-strand", s)
+      res <- processx::run(bedtools, args)
+      data.table::fread(res$stdout)
+    })
+    names(result) <- names(strand_map)
+    return(result)
+  }
+
+  res <- processx::run(bedtools, base_args)
+  return(data.table::fread(res$stdout))
 }
+
+# HELPERS =========
